2fbdfb39b4bead53adc2ab8cf1244d62c0a7e023
jordanMellish <jordanjmellish@gmail.com>
2018-08-14 15:15:40 +1000

    fix formatting

--- password_entry.py
+++ password_entry.py
@@ -4,6 +4,7 @@ Jordan
 
 MIN_LENGTH = 2
 
+
 def main():
     password = input('Enter password')
     while not is_valid_password(password):
@@ -12,9 +13,11 @@ def main():
     for char in password:
         print('*', end="")
 
+
 def is_valid_password(password):
     if len(password) < MIN_LENGTH:
         return False
     return True
+
 
 main()
