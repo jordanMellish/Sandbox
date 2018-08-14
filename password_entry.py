eedc73bfa8c18662fc303c03ceb88d857a53a6f2
jordanMellish <jordanjmellish@gmail.com>
2018-08-14 15:15:13 +1000

    add password entry program

--- password_entry.py
+++ password_entry.py
@@ -2,5 +2,19 @@
 Jordan
 """
 
+MIN_LENGTH = 2
+
 def main():
-    return
+    password = input('Enter password')
+    while not is_valid_password(password):
+        print('Invalid password,try again')
+        password = input('Enter password')
+    for char in password:
+        print('*', end="")
+
+def is_valid_password(password):
+    if len(password) < MIN_LENGTH:
+        return False
+    return True
+
+main()
